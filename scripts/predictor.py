cfba58f3be62ee66534bcf0ee5676c2a49302124
shkwsk <ren7link@gmail.com>
2019-02-18 22:12:50 +0900

    mod: second submit

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -9,32 +9,36 @@ test_df = pd.read_csv('../input/test.csv', header=0)
 # convert sex to categorical number
 train_df['Gender'] = train_df['Sex'].map({'female':0, 'male':1}).astype(int)
 test_df['Gender'] = test_df['Sex'].map({'female':0, 'male':1}).astype(int)
-print(train_df.head(3))
-print(test_df.head(3))
+# print(train_df.head(3))
+# print(test_df.head(3))
 
-# complement the missing values of age column with average of age
-median_age = train_df['Age'].dropna().median()
-if len(train_df.Age[train_df.Age.isnull()]) > 0:
-    train_df.loc[(train_df.Age.isnull()), 'Age'] = median_age
-
-median_age = test_df['Age'].dropna().median()
-if len(test_df.Age[test_df.Age.isnull()]) > 0:
-    test_df.loc[(test_df.Age.isnull()), 'Age'] = median_age
+# complement the missing values of column
+# age
+train_df.Age.fillna(train_df.Age.dropna().median(), inplace=True)
+test_df.Age.fillna(test_df.Age.dropna().median(), inplace=True)
+# fare
+train_df['Fare'].fillna(train_df['Fare'].dropna().median(), inplace=True)
+test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
 
 # copy id
 ids = test_df.PassengerId.values
 
 # remove un-used columns
-train_df = train_df.drop(['Name','Ticket','Sex','SibSp','Parch','Fare','Cabin','Embarked','PassengerId'], axis=1)
-test_df = test_df.drop(['Name','Ticket','Sex','SibSp','Parch','Fare','Cabin','Embarked','PassengerId'], axis=1)
+train_df = train_df.drop(['Name','Ticket','Sex','SibSp','Parch','Cabin','Embarked','PassengerId'], axis=1)
+test_df = test_df.drop(['Name','Ticket','Sex','SibSp','Parch','Cabin','Embarked','PassengerId'], axis=1)
 print(train_df.head(3))
 print(test_df.head(3))
 
-# predict with random forest
-train_data = train_df.values
-test_data = test_df.values
-model = RandomForestClassifier(n_estimators=200)
-survived = model.fit(train_data[0::, 1::], train_data[0::, 0]).predict(test_data).astype(int)
+# instance of random forest
+rfc = RandomForestClassifier(n_estimators=100)
+
+# training
+x_train = train_df.drop('Survived', axis=1)
+y_train = train_df['Survived']
+rfc.fit(x_train, y_train)
+
+# predict
+survived = rfc.predict(test_df).astype(int)
 
 # export result
 output_df = pd.DataFrame({
