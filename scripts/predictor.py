5aafdfd651ace8e1d495bdff190071637b46cd1f
shkwsk <ren7link@gmail.com>
2019-02-19 00:39:53 +0900

    add: except 'Name','Ticket','Cabin','PassengerId'

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -6,9 +6,14 @@ from sklearn.ensemble import RandomForestClassifier
 train_df = pd.read_csv('../input/train.csv', header=0)
 test_df = pd.read_csv('../input/test.csv', header=0)
 
-# convert sex to categorical number
-train_df['Gender'] = train_df['Sex'].map({'female':0, 'male':1}).astype(int)
-test_df['Gender'] = test_df['Sex'].map({'female':0, 'male':1}).astype(int)
+# convert to categorical number
+# sex
+train_df['Sex'] = train_df['Sex'].map({'female':0, 'male':1}).astype(int)
+test_df['Sex'] = test_df['Sex'].map({'female':0, 'male':1}).astype(int)
+# embarked
+freq_port = train_df.Embarked.dropna().mode()[0]
+train_df['Embarked'] = train_df['Embarked'].fillna(freq_port).map({'S':0,'C':1,'Q':2}).astype(int)
+test_df['Embarked'] = test_df['Embarked'].fillna(freq_port).map({'S':0,'C':1,'Q':2}).astype(int)
 # print(train_df.head(3))
 # print(test_df.head(3))
 
@@ -17,15 +22,15 @@ test_df['Gender'] = test_df['Sex'].map({'female':0, 'male':1}).astype(int)
 train_df.Age.fillna(train_df.Age.dropna().median(), inplace=True)
 test_df.Age.fillna(test_df.Age.dropna().median(), inplace=True)
 # fare
-train_df['Fare'].fillna(train_df['Fare'].dropna().median(), inplace=True)
-test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
+train_df.Fare.fillna(train_df.Fare.dropna().median(), inplace=True)
+test_df.Fare.fillna(test_df.Fare.dropna().median(), inplace=True)
 
 # copy id
 ids = test_df.PassengerId.values
 
 # remove un-used columns
-train_df = train_df.drop(['Name','Ticket','Sex','SibSp','Parch','Cabin','Embarked','PassengerId'], axis=1)
-test_df = test_df.drop(['Name','Ticket','Sex','SibSp','Parch','Cabin','Embarked','PassengerId'], axis=1)
+train_df = train_df.drop(['Name','Ticket','Cabin','PassengerId'], axis=1)
+test_df = test_df.drop(['Name','Ticket','Cabin','PassengerId'], axis=1)
 print(train_df.head(3))
 print(test_df.head(3))
 
